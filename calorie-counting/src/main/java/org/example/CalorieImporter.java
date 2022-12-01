fc3afc0aa3a49833e247ea6b9d111e8b7f74550b
Robert Dupplaw <dupplawrobert@gmail.com>
2022-12-01 19:41:27 +0000

    Messing around refactoring

---
+++ calorie-counting/src/main/java/org/example/CalorieImporter.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.nio.file.Files.readString;
+import static java.util.Arrays.stream;
+import static java.util.Objects.requireNonNull;
+
+public class CalorieImporter {
+
+    private final String resourceFilename;
+    private final String elfSeparator;
+    private final String calorieSeparator;
+
+    public CalorieImporter(String resourceFilename, String elfSeparator, String calorieSeparator) {
+        this.resourceFilename = resourceFilename;
+        this.elfSeparator = elfSeparator;
+        this.calorieSeparator = calorieSeparator;
+    }
+
+    List<Integer> parseCalorieTotals() throws URISyntaxException, IOException {
+        return stream(getInput().split(elfSeparator))
+                .map(s -> s.split(calorieSeparator))
+                .map(strings -> stream(strings)
+                        .mapToInt(Integer::parseInt)
+                        .sum())
+                .collect(Collectors.toList());
+    }
+
+    String getInput() throws URISyntaxException, IOException {
+        return readString(new File(requireNonNull(getClass().getClassLoader().getResource(resourceFilename)).toURI()).toPath());
+    }
+}
---
+++ calorie-counting/src/main/java/org/example/SnackAnalyzer.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static java.util.Comparator.reverseOrder;
+
+public class SnackAnalyzer {
+
+    private final CalorieImporter calorieImporter;
+
+    public SnackAnalyzer(CalorieImporter calorieImporter) {
+        this.calorieImporter = calorieImporter;
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        CalorieImporter calorieImporter = new CalorieImporter("input.txt", "\\n{2,}", "\\n");
+        System.out.println(new SnackAnalyzer(calorieImporter).threeHighestCaloriesTotal());
+    }
+
+    private int threeHighestCaloriesTotal() throws URISyntaxException, IOException {
+        return calorieImporter.parseCalorieTotals().stream()
+                .sorted(reverseOrder())
+                .limit(3)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
